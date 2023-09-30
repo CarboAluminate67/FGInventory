18bba6957d8d7bb36df772d91d34d0fefb25cf29
Alex Nielsen <alex.tommy.nielsen@gmail.com>
2023-09-30 02:12:09 -0400

    Begun Class dev

---
+++ FGInventory/FGInventory/Item.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FGInventory
+{
+    public class Item
+    {
+        private int _quantity;
+        private string _name;
+
+        public Item(string name, int quant)
+        {
+            _quantity = quant;
+            _name = name;
+        }
+
+        public void AddItems(int quant)
+        {
+            _quantity += quant;
+        }
+
+        public void NewQuant(int quant)
+        {
+            _quantity = quant;
+        }
+
+        public int GetQuant()
+        {
+            return _quantity;
+        }
+
+        public int GetName()
+        {
+            return _name;
+        }
+    }
+}
---
+++ FGInventory/FGInventory/Perish.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FGInventory
+{
+    public class Perish : Item
+    {
+        private int _expireTime;
+
+        public Perish(string name, int quant, int exp) : base(name, quant)
+        {
+            _expireTime = exp;
+        }
+    }
+}
